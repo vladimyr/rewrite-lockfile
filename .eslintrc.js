fad30b03f97eed47ed76501e5090d5c7a7d106af
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-30 00:43:29 +0200

    Use own ESLint config :wrench:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
+'use strict';
+
 module.exports = {
-  extends: 'semistandard',
-  rules: {
-    'prefer-const': 'error',
-    'space-before-function-paren': ['error', {
-      anonymous: 'always',
-      named: 'never'
-    }]
-  }
+  extends: '@vladimyr'
 };
